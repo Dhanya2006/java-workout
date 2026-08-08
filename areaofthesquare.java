a24fa3e091201a07009d702bf13adecd513dac71
Dhanya Anbukkarasu <146648747+Dhanya2006@users.noreply.github.com>
2026-08-08 22:27:40 +0530

    Add files via upload

---
+++ areaofthesquare.java
@@ -0,0 +1,13 @@
+package corejava;
+import java.util.Scanner;
+public class areaofthesquare {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a square value:");
+		int side = sc.nextInt();
+		System.out.println("area of the square:" +side*side);
+
+	}
+
+}
