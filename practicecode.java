eb24dba217e2e3e6b364890ff9771e9b36847fdc
Dhanya Anbukkarasu <146648747+Dhanya2006@users.noreply.github.com>
2026-08-08 22:28:09 +0530

    Add files via upload

---
+++ practicecode.java
@@ -0,0 +1,19 @@
+package corejava;
+import java.util.Scanner;
+public class practicecode {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a first number:");
+		//int a = sc.nextInt();
+		float a = sc.nextFloat();
+		System.out.println("enter a second number:");
+		//int b = sc.nextInt();
+		float b = sc.nextFloat();
+		//System.out.println("sum of the two number:" + (a+b));
+		System.out.println("average a two number:" + (a+b)/2);
+		
+
+	}
+
+}
